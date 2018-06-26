37ca086c5a5d79f24501306050ba81b5891e76d1
Prashant Gupta <prashantgupta0206@gmail.com>
2018-06-26 19:44:26 +0530

    Updated By Prashant

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
 const fs = require('fs'); 
+var express = require("express");
+var bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+var app = express();
+//app.use(express.bodyParser());
+app.use(fileUpload());
 
 let newArr; 
 let readData = [];
 let splitCount = 200000;
 
-fs.readFile('input.txt', function(err, data) {
-    if (err) {
-        throw err; 
-    }else {
-         newArr = data.toString().split('\n'); 
-         var count = 1; 
-        for (let i = 0; i <= newArr.length-1; i++ ) {
-            readData.push(newArr[i]); 
-            if (readData.length == splitCount || newArr.length-1 == i) {
-                writeData(readData, count); 
-                readData = [];
-                count++;
-            }
-            
-        }
-    }
+app.use(express.bodyParser({uploadDir:'./uploads'}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.post('/myForm', readeInput);
+app.listen(3000);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
 })
+
+function readeInput(req, res){
+    console.log(req.files.filename.path);
+    // fs.readFile('input.txt', function(err, data) {
+    //     if (err) {
+    //         throw err; 
+    //     }else {
+    //          newArr = data.toString().split('\n'); 
+    //          var count = 1; 
+    //         for (let i = 0; i <= newArr.length-1; i++ ) {
+    //             readData.push(newArr[i]); 
+    //             if (readData.length == splitCount || newArr.length-1 == i) {
+    //                 writeData(readData, count); 
+    //                 readData = [];
+    //                 count++;
+    //             }
+                
+    //         }
+    //     }
+    // })
+}
+
 
 function writeData(readData, count) {
     let file = fs.createWriteStream('output_'+count+'.txt'); 
